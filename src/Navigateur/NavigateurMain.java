0e70e2bfb45f60992fa768f2999a4192868c89ad
huang46u <qijia.huang2@etu.univ-lorraine.fr>
2016-11-15 20:44:31 +0100

    Menu

--- src/Navigateur/NavigateurMain.java
+++ src/Navigateur/NavigateurMain.java
@@ -1,8 +1,18 @@
 package Navigateur;
 
 import java.awt.Dimension;
+import java.awt.FileDialog;
+import java.awt.Menu;
+import java.awt.MenuBar;
+import java.awt.MenuItem;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 
 import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
@@ -51,21 +61,90 @@ public class NavigateurMain extends JFrame {
 				}.start();
 			}
 		});
-		frame.setTitle(TITLE);
-		frame.pack();
-		frame.setVisible(true);
-		animator.start();
-	}
+
+		MenuBar bar;  
+		Menu fileMenu;  
+		MenuItem closeItem; 
+		MenuItem openItem;  
+		FileDialog openDia;		     
+
+		bar = new MenuBar();  
+		fileMenu = new Menu("Fichier"); 
+
+		openItem = new MenuItem("Ouvrir");     
+		closeItem = new MenuItem("Exit"); 
+
+		fileMenu.add(openItem);     
+		fileMenu.add(closeItem);  
+		bar.add(fileMenu);  
+
+		frame.setMenuBar(bar);  
+
+		//默认模式为 FileDialog.LOAD  
+		openDia = new FileDialog(frame,"Ouvrir",FileDialog.LOAD);
+
+		openItem.addActionListener(new ActionListener()  
+		{  
+			//设置打开文件功能  
+			public void actionPerformed(ActionEvent e)  
+			{  
+				openDia.setVisible(true);  
+				String dirPath = openDia.getDirectory();//获取文件路径  
+				String fileName = openDia.getFile();//获取文件名称  
+				//System.out.println(dirPath +"++"+ fileName);  
+
+				//如果打开路径 或 目录为空 则返回空  
+				if(dirPath == null || fileName == null)  
+					return ;  
+
+				File file = new File(dirPath,fileName);  
+
+				try  
+				{  
+					BufferedReader bufr = new BufferedReader(new FileReader(file));    
+				}  
+				catch (IOException ex)  
+				{  
+					throw new RuntimeException("文件读取失败！");  
+				}  
 
 
-	public static void main(String[] args) {
-		SwingUtilities.invokeLater(new Runnable() {
-			public void run() {
-				new NavigateurMain();
-			}
-		});
 
-	}
+			}  
+		});  
+
+		closeItem.addActionListener(new ActionListener()  
+		{  
+			//设置退出功能  
+			public void actionPerformed(ActionEvent e)  
+			{  
+				System.exit(0);  
+			}  
+		});  
+
+		frame.addWindowListener(new WindowAdapter()  
+		{  
+			public void windowClosing(WindowEvent e)  
+			{  
+				System.exit(0);  
+			}  
+		});  
+	  
+	frame.setTitle(TITLE);
+	frame.pack();
+	frame.setVisible(true);
+	animator.start();
+}
+
+
+public static void main(String[] args) {
+	SwingUtilities.invokeLater(new Runnable() {
+		public void run() {
+			new NavigateurMain();
+		}
+	});
+
+}
 
 }
 
